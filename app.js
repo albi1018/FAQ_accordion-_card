ab47dcdb7c75b2ec742ef72344827e4ee49baebf
albi1018 <albi1018@gmail.com>
2022-02-14 16:09:31 +0100

    ap.js

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const accordions = document.querySelectorAll(".accordion__content");
 const arrows = document.querySelectorAll(".accordion__img");
 const texts = document.querySelectorAll(".show");
 
-accordions.forEach(accordion => {
-    accordion.addEventListener('click', function(){
-        this.classList.toggle('active');
+
+arrows.forEach(arrow => {
+    arrow.addEventListener("click", function() {
+        arrow.parentElement.classList.toggle('active');
     })
 })
+
+
